b3652026a1ff6f7602217a2c52d13d222186c12f
JOAO VITOR PRIMO DE OLIVEIRA <jv.primo26@gmail.com>
2022-07-29 14:46:48 -0300

    escolha do filme ok - tela 1

--- src/components/GlobalMovies/GlobalMovies.js
+++ src/components/GlobalMovies/GlobalMovies.js
@@ -1,25 +1,32 @@
 import "./style.css"
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default function GlobalMovies(){
-const [movies, setMovies] = useState ([]);
+export default function GlobalMovies() {
+    const [movies, setMovies] = useState([]);
 
-useEffect(()=>  {
-const promise = axios.get("https://mock-api.driven.com.br/api/v7/cineflex/movies");
-promise.then((request) =>{
-    setMovies(request.data)
-});
-}, []);
-return (
+    useEffect(() => {
+        const promise = axios.get("https://mock-api.driven.com.br/api/v7/cineflex/movies");
+        promise.then((request) => {
+            setMovies(request.data)
+        });
+    }, []);
+    return (
 
-<div className="container">
-<h2>Selecione o Filme</h2>    
-<div className="playing">
-{movies.map(movie => <img key={movie.id} src={movie.posterURL} alt="" /> )}
-</div>
-</div>
+        <div className="container">
+            <h2>Selecione o Filme</h2>
+            <div className="playing">
 
-)
+                {movies.map(movie => <div key={movie.id} className="movie">
+                    <Link to={`/sessoes/${movie.id}`}>
+                        <img key={movie.id} src={movie.posterURL} alt="" />
+                    </Link>
+                </div>)}
+
+            </div>
+        </div>
+
+    )
 
 }
